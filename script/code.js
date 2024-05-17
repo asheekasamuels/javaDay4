932ec5bc03fff04a83899268b42391eb4d0aeb96
asheekasamuels <samuelsasheeka133@gmail.com>
2024-05-17 10:20:34 +0200

    day4

---
+++ script/code.js
@@ -0,0 +1,35 @@
+// Create the first object
+let person1 = {
+    name: "Asheeka",
+    age: 23,
+    city: "Cape Town"
+};
+
+// Create the second object with different values
+let person2 = {
+    name: "Zulkifl",
+    age: 20,
+    city: "Cape Town"
+};
+
+// Create the third object with different values
+let person3 = {
+    name: "Jamie",
+    age: 24,
+    city: "Cape Town"
+};
+
+// Log the first object
+console.log(person1);
+
+// Log the second object
+console.log(person2);
+
+// Log the third object
+console.log(person3);
+
+// Create an array and add the objects to it
+let people = [person1, person2, person3];
+
+// Log the array
+console.log(people);
